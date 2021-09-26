0c509eddc4cdb99458fa2da5d816460f38c70abe
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-09-26 20:12:44 +0530

    UI update

--- pages/add.js
+++ pages/add.js
@@ -14,13 +14,13 @@ const add = () => {
           integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
           crossOrigin="anonymous"
         />
+      </Head>
+      <div className="container">
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
           crossOrigin="anonymous"
         ></Script>
-      </Head>
-      <div className="container">
         <div className="row">
           <div className="col-md-3 col-sm-0"></div>
           <div className="col-md-6 col-sm-12 text-center">
