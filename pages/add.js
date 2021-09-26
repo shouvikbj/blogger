4c8eeac4199363a16cd355a47c8e3a7aa2e05b19
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-09-26 20:11:28 +0530

    UI update

--- pages/add.js
+++ pages/add.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 
 import Head from "next/head";
+import Script from "next/script";
 
 const add = () => {
   return (
@@ -13,11 +14,11 @@ const add = () => {
           integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
           crossOrigin="anonymous"
         />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
           crossOrigin="anonymous"
-        ></script>
+        ></Script>
       </Head>
       <div className="container">
         <div className="row">
